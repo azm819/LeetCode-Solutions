84ff53e15e0d8e1b010e787dfb9b2903d386052e
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2022-09-11 16:27:41 +0000

    Sync LeetCode submission - Add Two Numbers (swift)

--- 0002🟡Add_Two_Numbers.swift
+++ 0002🟡Add_Two_Numbers.swift
@@ -8,7 +8,6 @@
  *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
  * }
  */
-
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
         var t1 = l1
@@ -19,25 +18,21 @@ class Solution {
         while t1 != nil || t2 != nil {
             let sum = (t1?.val ?? .zero) + (t2?.val ?? .zero) + ad
             ad = sum / 10
-            let old = res
-            res = ListNode(sum % 10)
             if absRes == nil {
-                absRes = res
+                absRes = ListNode(sum % 10)
+                res = absRes
+            } else {
+                res?.next = ListNode(sum % 10)
+                res = res?.next
             }
-            old?.next = res
-            
+
             t1 = t1?.next
             t2 = t2?.next
         }
         if ad == 1 {
-            let old = res
-            res = ListNode(1)
-            if absRes == nil {
-                absRes = res
-            }
-            old?.next = res
+            res?.next = ListNode(1)
         }
-        
+
         return absRes
     }
 }
