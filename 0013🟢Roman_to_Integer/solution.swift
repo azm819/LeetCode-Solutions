e0e35d86c91750b4629a19a461bf6c753299eede
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2022-09-24 21:32:48 +0000

    Sync LeetCode submission Runtime - 52 ms (5.31%), Memory - 14.6 MB (100.00%)

--- 0013🟢Roman_to_Integer/solution.swift
+++ 0013🟢Roman_to_Integer/solution.swift
@@ -1,38 +1,18 @@
 class Solution {
-    func romanToInt(_ c: Character) -> Int {
-        switch c {
-        case "I": return 1
-        case "V": return 5
-        case "X": return 10
-        case "L": return 50
-        case "C": return 100
-        case "D": return 500
-        case "M": return 1000
-        default: return 0
-        }
-    }
+    private let numerals: [Character: Int] = [
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    ]
 
     func romanToInt(_ s: String) -> Int {
-        var result = 0
-        var ind = s.startIndex
-        var lastInt = romanToInt(s[ind])
-        ind = s.index(after: ind)
-        while ind != s.endIndex {
-            let currInt = romanToInt(s[ind])
-            if lastInt >= currInt {
-                result += lastInt
-                lastInt = currInt
-                ind = s.index(after: ind)
-            } else {
-                result += currInt - lastInt
-                ind = s.index(after: ind)
-                lastInt = 0
-                if ind == s.endIndex { break }
-                lastInt = romanToInt(s[ind])
-                ind = s.index(after: ind)
-            }
-        }
-        result += lastInt
-        return result
+        return s.compactMap { numerals[$0] }
+            .reversed()
+            .reduce((0, 0)) {$0.1 <= $1 ? ($0.0 + $1, $1) : ($0.0 - $1, 0) }
+            .0
     }
 }
