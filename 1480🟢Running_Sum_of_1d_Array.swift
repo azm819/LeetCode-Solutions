e9f025300fa9e94dc75409ac2da9a3510167ae28
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-01-11 13:47:15 +0000

    Sync LeetCode submission - Running Sum of 1d Array (swift)

---
+++ 1480🟢Running_Sum_of_1d_Array.swift
@@ -0,0 +1,10 @@
+class Solution {
+    func runningSum(_ nums: [Int]) -> [Int] {
+        guard nums.count > 1 else { return nums }
+        var res = nums
+        for i in 1..<res.count {
+            res[i] += res[i - 1]
+        }
+        return res
+    }
+}
