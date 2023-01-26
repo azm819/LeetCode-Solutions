4cd5cd0410cd2274832848031888e621758a1281
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-01-26 21:35:06 +0000

    Sync LeetCode submission - Happy Number (swift)

---
+++ 0202🟢Happy_Number.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func isHappy(_ n: Int) -> Bool {
+        var n = n
+        var old = Set<Int>()
+        while n != 1 {
+            var res = 0
+            old.insert(n)
+            while n > 0 {
+                let digit = n % 10
+                res += digit * digit
+                n /= 10
+            }
+            if old.contains(res) {
+                return false
+            }
+            n = res
+        }
+        // print(old)
+        return true
+    }
+}
