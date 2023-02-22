5260c840128bdefcd1116a5e20f9f9cd9c7ea817
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-02-22 07:42:40 +0000

    Sync LeetCode submission - Capacity To Ship Packages Within D Days (swift)

---
+++ 1011🟡Capacity_To_Ship_Packages_Within_D_Days.swift
@@ -0,0 +1,36 @@
+class Solution {
+    private func check(_ weights: [Int], _ days: Int, _ cap: Int) -> Bool {
+        var day = 1
+        var curCap = 0
+        for weight in weights {
+            if curCap + weight > cap {
+                day += 1
+                curCap = weight
+            } else {
+                curCap += weight
+            }
+            if day > days {
+                return false
+            }
+        }
+        return day <= days
+    }
+
+    func shipWithinDays(_ weights: [Int], _ days: Int) -> Int {
+        var l = 0
+        var r = 0
+        for weight in weights {
+            l = max(l, weight)
+            r += weight
+        }
+        while l <= r {
+            let c = (l + r) / 2
+            if check(weights, days, c) {
+                r = c - 1
+            } else {
+                l = c + 1
+            }
+        }
+        return l
+    }
+}
