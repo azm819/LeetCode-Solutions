1c2db8f296deb794983b3831f4046297427030ad
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-03-20 08:55:35 +0000

    Sync LeetCode submission - Can Place Flowers (swift)

---
+++ 0605🟢Can_Place_Flowers.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
+        var wasZero = true
+        var result = 0
+        for i in 0..<flowerbed.count {
+            if flowerbed[i] == 0 {
+                if wasZero && (i == flowerbed.count - 1 || flowerbed[i + 1] == 0) {
+                    result += 1
+                    wasZero = false
+                } else {
+                    wasZero = true
+                }
+            } else if flowerbed[i] == 1 {
+                wasZero = false
+            }
+        }
+        return result >= n
+    }
+}
