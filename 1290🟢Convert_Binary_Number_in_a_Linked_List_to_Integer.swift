d69337fd87e721df057ea45a23abf7c8526c212e
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-03-23 10:04:02 +0000

    Sync LeetCode submission - Convert Binary Number in a Linked List to Integer (swift)

---
+++ 1290🟢Convert_Binary_Number_in_a_Linked_List_to_Integer.swift
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func getDecimalValue(_ head: ListNode?) -> Int {
+        var cur = head
+        var result = 0
+        while let next = cur {
+            result = result * 2 + next.val
+            cur = next.next
+        }
+        return result
+    }
+}
