a29164fc5164a39f63a56c62b4631f83c5619460
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-04-19 06:39:25 +0000

    Sync LeetCode submission - Sqrt(x) (swift)

--- 0069🟢Sqrt(x).swift
+++ 0069🟢Sqrt(x).swift
@@ -1,12 +1,18 @@
 class Solution {
     func mySqrt(_ x: Int) -> Int {
-        var result = 0
-        while result * result < x {
-            result += 1
+        var l = 0
+        var r = x
+        while l < r {
+            let c = (l + r + 1) / 2
+            let square = c * c
+            if square == x {
+                return c
+            } else if square < x {
+                l = c
+            } else {
+                r = c - 1
+            }
         }
-        if result * result > x {
-            result -= 1
-        }
-        return result
+        return l
     }
 }
