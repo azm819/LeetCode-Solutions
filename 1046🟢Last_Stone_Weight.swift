7bbefe38480dd0a0d9fb5fef34207292cc5421ea
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-04-24 06:13:27 +0000

    Sync LeetCode submission - Last Stone Weight (swift)

--- 1046🟢Last_Stone_Weight.swift
+++ 1046🟢Last_Stone_Weight.swift
@@ -36,19 +36,6 @@ class Solution {
         shiftUp(heap.count - 1, &heap)
     }
 
-    private func isValid(_ heap: [Int], _ ind: Int) -> Bool {
-        guard ind < heap.count else { return true }
-        let left = 2 * ind + 1
-        if left < heap.count && heap[left] > heap[ind] {
-            return false
-        }
-        let right = 2 * ind + 2
-        if right < heap.count && heap[right] > heap[ind] {
-            return false
-        }
-        return isValid(heap, left) && isValid(heap, right)
-    }
-
     func lastStoneWeight(_ stones: [Int]) -> Int {
         guard stones.count > 1 else { return stones[0] }
         guard stones.count > 2 else { return abs(stones[0] - stones[1]) }
@@ -57,7 +44,6 @@ class Solution {
             insert(stone, &heap)
         }
         while true {
-            // print(isValid(heap, 0))
             let first = heap[0]
             let secInd = heap[1] > heap[2] ? 1 : 2
             let second = heap[secInd]
