ae2269497b234b37933d3ce6bf327e223257107c
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-04-24 18:31:42 +0000

    Sync LeetCode submission - Kth Missing Positive Number (swift)

--- 1539🟢Kth_Missing_Positive_Number.swift
+++ 1539🟢Kth_Missing_Positive_Number.swift
@@ -1,15 +1,26 @@
 class Solution {
     func findKthPositive(_ arr: [Int], _ k: Int) -> Int {
-        var lastNum = 0
-        var curK = 0
-        for int in arr {
-            let diff = int - lastNum - 1
-            curK += diff
-            if curK >= k {
-                return lastNum + (diff - (curK - k))
-            }
-            lastNum = int
+        var l = 0
+        var r = arr.count - 1
+        var curK = k - arr[0] + 1
+        guard curK > .zero else { return k }
+        let diff = (arr[r] - arr[l]) - (r - l)
+        guard diff >= curK else {
+            return arr[r] + (curK - diff)
         }
-        return lastNum + (k - curK)
+        while l < r {
+            let c = (l + r + 1) / 2
+            let diff = (arr[c] - arr[l]) - (c - l)
+            if diff < curK {
+                curK -= diff
+                l = c
+            } else {
+                if l + 1 == r {
+                    return arr[l] + curK
+                }
+                r = c
+            }
+        }
+        return arr[r]
     }
 }
