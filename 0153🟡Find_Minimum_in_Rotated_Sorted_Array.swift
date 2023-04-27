48552379cf1b2ed3ec82cf64eac11706468e9d00
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-04-27 14:15:32 +0000

    Sync LeetCode submission - Find Minimum in Rotated Sorted Array (swift)

--- 0153🟡Find_Minimum_in_Rotated_Sorted_Array.swift
+++ 0153🟡Find_Minimum_in_Rotated_Sorted_Array.swift
@@ -1,28 +1,20 @@
 class Solution {
     func findMin(_ nums: [Int]) -> Int {
         var l = 0
-        let n = nums.count - 1
-        var r = n
-        if nums[l] <= nums[r] {
-            return nums[l]
-        }
-        while l <= r {
+        var r = nums.count - 1
+        while l < r {
             let c = (l + r) / 2
             let val = nums[c]
-            if c > 0 && val < nums[c - 1] {
-                return val
-            }
-
-            if c <= n && val > nums[c + 1] {
-                return nums[c + 1]
-            }
-
-            if nums[l] <= val {
-                l = c + 1
+            if nums[c] >= nums[l] {
+                if nums[r] >= nums[c] {
+                    r = c - 1
+                } else {
+                    l = c + 1
+                }
             } else {
                 r = c
             }
         }
-        return -5000
+        return nums[l]
     }
 }
