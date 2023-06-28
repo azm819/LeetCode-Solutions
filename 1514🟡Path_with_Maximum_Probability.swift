fef2eac6860795710d06c024c0406e8ee7e8e729
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-06-28 22:26:57 +0000

    Sync LeetCode submission - Path with Maximum Probability (swift)

---
+++ 1514🟡Path_with_Maximum_Probability.swift
@@ -0,0 +1,32 @@
+class Solution {
+    func maxProbability(_ n: Int, _ edges: [[Int]], _ succProb: [Double], _ start: Int, _ end: Int) -> Double {
+        var neighbours = [Int: [(Int, Double)]]()
+        for i in 0..<edges.count {
+            neighbours[edges[i][0], default: []].append((edges[i][1], succProb[i]))
+            neighbours[edges[i][1], default: []].append((edges[i][0], succProb[i]))
+        }
+        var probs = Array(repeating: Double.zero, count: n)
+        probs[start] = 1
+        var queue = [start]
+        var inQueue = Array(repeating: false, count: n)
+        while let cur = queue.popLast() {
+            inQueue[cur] = false
+            var updated = false
+            for (neigh, prob) in neighbours[cur, default: []] {
+                let newProb = probs[cur] * prob
+                if newProb > probs[neigh] {
+                    probs[neigh] = newProb
+                    updated = true
+                    if !inQueue[neigh] {
+                        queue.append(neigh)
+                    }
+                }
+            }
+            if updated {
+                queue.sort { probs[$0] < probs[$1] }
+            }
+        }
+        return probs[end]
+    }
+}
+
