fb13bf684cc945194a9d6554f2f0d912bdaae3e2
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-07-05 21:30:46 +0000

    Sync LeetCode submission - Binary Tree Preorder Traversal (swift)

---
+++ 0144🟢Binary_Tree_Preorder_Traversal.swift
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+        guard let root = root else { return [] }
+        var result = [Int]()
+        var queue = [root]
+        while let node = queue.popLast() {
+            result.append(node.val)
+            if let right = node.right {
+                queue.append(right)
+            }
+            if let left = node.left {
+                queue.append(left)
+            }
+        }
+        return result
+    }
+}
