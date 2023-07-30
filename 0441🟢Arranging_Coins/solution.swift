d35bc2854a3d55192144855395e11502efa64c50
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-07-30 18:40:22 +0000

    Sync LeetCode submission Runtime - 4 ms (91.49%), Memory - 13.4 MB (100.00%)

--- 0441🟢Arranging_Coins/solution.swift
+++ 0441🟢Arranging_Coins/solution.swift
@@ -4,18 +4,15 @@ class Solution {
         var r = n
         while l <= r {
             let c = (l + r) / 2
-            let maxSum = ((c + 1) / 2) * c + ((c + 1) % 2) * (c / 2)
+            let maxSum = c * (c + 1) / 2
             if n == maxSum {
                 return c
             } else if n > maxSum {
                 l = c + 1
             } else {
-                if n > maxSum - c {
-                    return c - 1
-                }
                 r = c - 1
             }
         }
-        return l
+        return r
     }
 }
