b2457666c68bd30b3a1f8636b26e25814052f894
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-08-01 08:33:06 +0000

    Sync LeetCode submission - Maximum Ice Cream Bars (swift)

--- 1833🟡Maximum_Ice_Cream_Bars.swift
+++ 1833🟡Maximum_Ice_Cream_Bars.swift
@@ -1,13 +1,16 @@
 class Solution {
     func maxIceCream(_ costs: [Int], _ coins: Int) -> Int {
-        let costs = costs.sorted()
-        var result = 0
-        var curSum = 0
+        var counts = Array(repeating: 0, count: 100_001)
         for cost in costs {
-            curSum += cost
-            if curSum <= coins {
-                result += 1
-            }
+            counts[cost] += 1
+        }
+        var result = 0
+        var remainingCoins = coins
+        for cost in 1 ... 100_000 {
+            if remainingCoins < cost { break }
+            let count = min(counts[cost], remainingCoins / cost)
+            result += count
+            remainingCoins -= count * cost
         }
         return result
     }
