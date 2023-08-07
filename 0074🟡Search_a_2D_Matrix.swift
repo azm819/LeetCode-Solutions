76ad1034252a794b54d54ed19d4eac0be8c1282e
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-08-07 06:49:25 +0000

    Sync LeetCode submission - Search a 2D Matrix (swift)

--- 0074🟡Search_a_2D_Matrix.swift
+++ 0074🟡Search_a_2D_Matrix.swift
@@ -1,23 +1,20 @@
 class Solution {
     func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        guard matrix[0][0] <= target && matrix.last!.last! >= target else { return false }
         var l = 0
         var r = matrix.count - 1
-        while l < r {
+        var row = 0
+        while l <= r {
             let c = (l + r) / 2
-            if matrix[c][0] == target {
-                return true
-            } else if matrix[c][0] < target {
-                if matrix[c + 1][0] > target {
-                    l = c
-                    break
-                } else {
-                    l = c + 1
-                }
-            } else {
+            if matrix[c][0] <= target && target <= matrix[c].last! {
+                row = c
+                break
+            } else if target < matrix[c][0] {
                 r = c - 1
+            } else {
+                l = c + 1
             }
         }
-        let row = l
         l = 0
         r = matrix[row].count - 1
         while l <= r {
