db717600279b960ad14e69e4c1a16ade105550fe
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-09-03 08:41:10 +0000

    Sync LeetCode submission Runtime - 3 ms (59.69%), Memory - 13.3 MB (100.00%)

--- 0062🟡Unique_Paths/solution.swift
+++ 0062🟡Unique_Paths/solution.swift
@@ -1,31 +1,24 @@
 class Solution {
-    struct Pair: Hashable {
-        let x: Int
-        let y: Int
-
-        static func == (lhs: Pair, rhs: Pair) -> Bool {
-            lhs.x == rhs.x && lhs.y == rhs.y || lhs.y == rhs.x && lhs.x == rhs.y
+    private func dp(
+        _ i: Int,
+        _ j: Int,
+        _ memo: inout [[Int?]]
+    ) -> Int {
+        guard i < memo.count && j < memo[0].count else {
+            return .zero
         }
-    }
-
-    private var comb = [Pair: Int]()
-
-    func uniquePaths(_ m: Int, _ n: Int) -> Int {
-        guard m > 1 && n > 1 else { return 1 }
-        var result = 0
-        let f = Pair(x: m - 1, y: n)
-        let s = Pair(x: m, y: n - 1)
-        if let c = comb[f] {
-            result += c
-        } else {
-            result += uniquePaths(m - 1, n)
+        if let mem = memo[i][j] {
+            return mem
         }
-        if let c = comb[s] {
-            result += c
-        } else {
-            result += uniquePaths(m, n - 1)
-        }
-        comb[Pair(x: m, y: n)] = result
+        let result = dp(i + 1, j, &memo) + dp(i, j + 1, &memo)
+        memo[i][j] = result
         return result
     }
+
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        var memo = Array(repeating: Array(repeating: Optional<Int>.none, count: m), count: n)
+        memo[n - 1][m - 1] = 1
+        return dp(.zero, .zero, &memo)
+    }
 }
+
