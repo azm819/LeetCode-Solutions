d3272cf56585183179d81520170f5a8c4b5ccc1a
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-09-24 11:15:51 +0000

    Sync LeetCode submission Runtime - 62 ms (5.38%), Memory - 14.2 MB (100.00%)

--- 0004🔴Median_of_Two_Sorted_Arrays/solution.swift
+++ 0004🔴Median_of_Two_Sorted_Arrays/solution.swift
@@ -1,11 +1,44 @@
 class Solution {
-    func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
-        let a = (nums1 + nums2).sorted()
-        if a.count % 2 == 0 {
-            let i = a.count / 2
-            return Double(a[i] + a[i - 1]) / 2
+    private func solve(
+        _ k: Int,
+        _ nums1: [Int],
+        _ l1: Int,
+        _ r1: Int,
+        _ nums2: [Int],
+        _ l2: Int,
+        _ r2: Int
+    ) -> Int {
+        guard l1 <= r1 else {
+            return nums2[k - l1]
+        }
+        guard l2 <= r2 else {
+            return nums1[k - l2]
+        }
+        let c1 = (l1 + r1) / 2
+        let c2 = (l2 + r2) / 2
+        if c1 + c2 < k {
+            if nums1[c1] > nums2[c2] {
+                return solve(k, nums1, l1, r1, nums2, c2 + 1, r2)
+            } else {
+                return solve(k, nums1, c1 + 1, r1, nums2, l2, r2)
+            }
         } else {
-            return Double(a[a.count / 2])
+            if nums1[c1] > nums2[c2] {
+                return solve(k, nums1, l1, c1 - 1, nums2, l2, r2)
+            } else {
+                return solve(k, nums1, l1, r1, nums2, l2, c2 - 1)
+            }
+        }
+    }
+
+    func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
+        let totalCount = nums1.count + nums2.count
+        let k = totalCount / 2
+        if totalCount % 2 == 0 {
+            return Double(solve(k, nums1, 0, nums1.count - 1, nums2, 0, nums2.count - 1) + solve(k - 1, nums1, 0, nums1.count - 1, nums2, 0, nums2.count - 1)) / 2
+        } else {
+            return Double(solve(k, nums1, 0, nums1.count - 1, nums2, 0, nums2.count - 1))
         }
     }
 }
+
