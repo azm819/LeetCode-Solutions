6d6a80c0bd83bdab6415498d70b5f4d6e777b2a7
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-11-06 13:03:23 +0000

    Sync LeetCode submission - Seat Reservation Manager (swift)

--- 1845🟡Seat_Reservation_Manager.swift
+++ 1845🟡Seat_Reservation_Manager.swift
@@ -1,19 +1,67 @@
 class SeatManager {
-    private var heap: [Int]
+    private var heap: [Int] = []
 
     init(_ n: Int) {
-        heap = Array(stride(from: n, through: 1, by: -1))
+        for seatNumber in 1 ... n {
+            insert(seatNumber, &heap)
+        }
     }
 
     func reserve() -> Int {
-        heap.removeLast()
+        popMin(&heap)
     }
 
     func unreserve(_ seatNumber: Int) {
-        heap.append(seatNumber)
-        heap.sort(by: >)
+        insert(seatNumber, &heap)
     }
 }
+
+private extension SeatManager {
+    func siftUp(_ ind: Int, _ heap: inout [Int]) {
+        let par = (ind - 1) / 2
+        if heap[ind] < heap[par] {
+            heap.swapAt(ind, par)
+            siftUp(par, &heap)
+        }
+    }
+
+    func siftDown(_ ind: Int, _ heap: inout [Int]) {
+        let left = 2 * ind + 1
+        guard left < heap.count else { return }
+        let right = 2 * ind + 2
+        if right < heap.count {
+            if heap[left] < heap[right] {
+                if heap[ind] > heap[left] {
+                    heap.swapAt(ind, left)
+                    siftDown(left, &heap)
+                }
+            } else {
+                if heap[ind] > heap[right] {
+                    heap.swapAt(ind, right)
+                    siftDown(right, &heap)
+                }
+            }
+        } else {
+            if heap[ind] > heap[left] {
+                heap.swapAt(ind, left)
+                siftDown(left, &heap)
+            }
+        }
+    }
+
+    func insert(_ key: Int, _ heap: inout [Int]) {
+        heap.append(key)
+        siftUp(heap.count - 1, &heap)
+    }
+
+    func popMin(_ heap: inout [Int], _ newHead: Int = .max) -> Int {
+        let minimum = heap[0]
+        heap[0] = newHead
+        siftDown(.zero, &heap)
+        return minimum
+    }
+}
+
 
 /**
  * Your SeatManager object will be instantiated and called as such:
