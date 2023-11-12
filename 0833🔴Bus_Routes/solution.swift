10971da2e2fcdc5deb54f0536dda3031b639c08e
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-11-12 10:59:22 +0000

    Sync LeetCode submission Runtime - 1173 ms (35.42%), Memory - 22.4 MB (100.00%)

--- 0833🔴Bus_Routes/solution.swift
+++ 0833🔴Bus_Routes/solution.swift
@@ -1,63 +1,33 @@
 class Solution {
     func numBusesToDestination(_ routes: [[Int]], _ source: Int, _ target: Int) -> Int {
-        guard source != target else { return .zero }
-        guard routes.count > 1 else {
-            return routes[0].contains(source) && routes[0].contains(target) ? 1 : -1
-        }
-        guard routes.count > 2 else {
-            let hasS0 = routes[0].contains(source)
-            let hasT0 = routes[0].contains(target)
-            if hasS0 && hasT0 { return 1 }
-            let hasS1 = routes[1].contains(source)
-            let hasT1 = routes[1].contains(target)
-            if hasS1 && hasT1 { return 1 }
-            if (hasS0 && hasT1 || hasS1 && hasT0) && !Set(routes[0]).isDisjoint(with: Set(routes[1])) {
-                return 2
-            }
-            return -1
-        }
         var buses = [Int: [Int]]()
-        for i in 0..<routes.count {
+        for i in 0 ..< routes.count {
             for stop in routes[i] {
                 buses[stop, default: []].append(i)
             }
         }
-        var visited = Set<Int>()
-        visited.insert(source)
-        var queue = Set<Int>()
-        buses[source]?
-            .flatMap { routes[$0] }
-            .forEach { queue.insert($0) }
-        var res = 1
-        defer {
-            buses.removeAll()
-            visited.removeAll()
-            queue.removeAll()
-        }
-        while !queue.isEmpty {
-            var newQueue = Set<Int>()
-            while let stop = queue.popFirst() {
-                if stop == target {
-                    return res
-                }
-                let (inserted, _) = visited.insert(stop)
-                if !inserted {
-                    continue
-                }
-                buses[stop]?
-                    .flatMap { routes[$0] }
-                    .forEach {
-                        if !visited.contains($0) {
-                            newQueue.insert($0)
-                        }
-                    }
-                buses[stop] = nil
-            }
-            queue = newQueue
 
-            res += 1
+        var result = 0
+        var queue = [source]
+        var visitedStops: Set<Int> = [source]
+        var visitedBuses: Set<Int> = []
+        while !queue.isEmpty {
+            var newQueue = [Int]()
+            for stop in queue {
+                if stop == target {
+                    return result
+                }
+                for bus in buses[stop, default: []] where !visitedBuses.contains(bus) {
+                    for next in routes[bus] where !visitedStops.contains(next) {
+                        newQueue.append(next)
+                        visitedStops.insert(stop)
+                    }
+                    visitedBuses.insert(bus)
+                }
+            }
+            result += 1
+            queue = newQueue
         }
         return -1
     }
 }
-
