13ec9858c98c431575e0edabfb4cd9e319425ec7
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-11-29 06:58:00 +0000

    Sync LeetCode submission Runtime - 47 ms (65.88%), Memory - 14.8 MB (100.00%)

---
+++ 3194🟢Find_Words_Containing_Character/solution.swift
@@ -0,0 +1,8 @@
+class Solution {
+    func findWordsContaining(_ words: [String], _ x: Character) -> [Int] {
+        words
+            .enumerated()
+            .filter { $0.element.contains(x) }
+            .map(\.offset)
+    }
+}
