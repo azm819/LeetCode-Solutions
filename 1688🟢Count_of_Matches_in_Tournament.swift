e49c7946c709fc92afe09484f22f9619d9f3ef6d
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-12-05 12:02:37 +0000

    Sync LeetCode submission - Count of Matches in Tournament (swift)

--- 1688🟢Count_of_Matches_in_Tournament.swift
+++ 1688🟢Count_of_Matches_in_Tournament.swift
@@ -1,12 +1,5 @@
 class Solution {
     func numberOfMatches(_ n: Int) -> Int {
-        var n = n
-        var result = 0
-        while n > 1 {
-            result += n % 2
-            n /= 2
-            result += n
-        }
-        return result
+        n - 1
     }
 }
