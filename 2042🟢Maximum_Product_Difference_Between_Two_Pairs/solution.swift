d8ecdc86ab4b145ab3425605950c2114e742a24a
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2023-12-18 06:50:02 +0000

    Sync LeetCode submission Runtime - 151 ms (8.11%), Memory - 14.4 MB (100.00%)

---
+++ 2042🟢Maximum_Product_Difference_Between_Two_Pairs/solution.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func maxProductDifference(_ nums: [Int]) -> Int {
+        var a: Int = .min
+        var b: Int = .min
+        var c: Int = .max
+        var d: Int = .max
+        for num in nums {
+            if num > a {
+                b = a
+                a = num
+            } else if num > b {
+                b = num
+            }
+
+            if num < c {
+                d = c
+                c = num
+            } else if num < d {
+                d = num
+            }
+        }
+        return a * b - c * d
+    }
+}
+
