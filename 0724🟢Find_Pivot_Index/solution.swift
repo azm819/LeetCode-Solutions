9bc2f95626792d77c00ea4baa28a2746f46974df
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-01-03 11:15:36 +0000

    Sync LeetCode submission Runtime - 131 ms (5.51%), Memory - 16 MB (5.51%)

--- 0724🟢Find_Pivot_Index/solution.swift
+++ 0724🟢Find_Pivot_Index/solution.swift
@@ -1,18 +1,16 @@
 class Solution {
     func pivotIndex(_ nums: [Int]) -> Int {
-        guard !nums.isEmpty else { return -1 }
         guard nums.count > 1 else { return 0 }
-        var left = 0
-        var right = -nums[0]
-        for i in nums {
-            right += i
+        var rightSum = -nums[0]
+        for num in nums {
+            rightSum += num
         }
         var ind = 0
-        // [1,7,3,6,5,6]
-        while ind < nums.count && left != right {
-            left += nums[ind]
+        var leftSum = 0
+        while ind < nums.count && leftSum != rightSum {
+            leftSum += nums[ind]
             if ind + 1 < nums.count {
-                right -= nums[ind + 1]
+                rightSum -= nums[ind + 1]
                 ind += 1
             } else {
                 return -1
