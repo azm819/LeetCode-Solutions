0557c97855a528582a24cba0e2302da0c4fd7164
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-01-03 11:24:26 +0000

    Sync LeetCode submission Runtime - 166 ms (5.19%), Memory - 16.2 MB (85.47%)

--- 1392🟢Find_the_Difference_of_Two_Arrays/solution.swift
+++ 1392🟢Find_the_Difference_of_Two_Arrays/solution.swift
@@ -3,26 +3,30 @@ class Solution {
         let rawValue: Int
 
         static let none = Source(rawValue: 0)
-        static let first = Source(rawValue: 1)
-        static let second = Source(rawValue: 2)
+        static let first = Source(rawValue: 1 << 0)
+        static let second = Source(rawValue: 1 << 1)
     }
 
     func findDifference(_ nums1: [Int], _ nums2: [Int]) -> [[Int]] {
         var appearances = [Int: Source]()
         for int in nums1 {
-            appearances[int, default: .none].insert(.first)
+            appearances[int] = .first
         }
         for int in nums2 {
             appearances[int, default: .none].insert(.second)
         }
         var result: [[Int]] = [[], []]
-        for (k, v) in appearances {
-            if v.rawValue == 1 {
-                result[0].append(k)
-            } else if v.rawValue == 2 {
-                result[1].append(k)
+        for (key, value) in appearances {
+            switch value {
+            case .first:
+                result[0].append(key)
+            case .second:
+                result[1].append(key)
+            default:
+                break
             }
         }
         return result
     }
 }
+
