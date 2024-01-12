a27d6675b4bc62746e7da65c9d7236b287efd116
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-01-12 07:45:31 +0000

    Sync LeetCode submission Runtime - 78 ms (7.77%), Memory - 17 MB (66.02%)

---
+++ 0450🟡Delete_Node_in_a_BST/solution.swift
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func deleteNode(_ root: TreeNode?, _ key: Int) -> TreeNode? {
+        guard let root = root else { return nil }
+
+        guard root.val == key else {
+            if root.val > key {
+                root.left = deleteNode(root.left, key)
+            } else {
+                root.right = deleteNode(root.right, key)
+            }
+            return root
+        }
+
+        guard let right = root.right else {
+            return root.left
+        }
+        guard let left = root.left else {
+            return right
+        }
+
+        var leftRight = left
+        while let leftRightRight = leftRight.right {
+            leftRight = leftRightRight
+        }
+        leftRight.right = right.left
+        right.left = left
+        return right
+    }
+}
