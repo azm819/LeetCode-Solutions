12ef09829f1464ad9b36c346836507cc98d0ca97
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-01-21 11:03:48 +0000

    Sync LeetCode submission Runtime - 5 ms (35.75%), Memory - 15.3 MB (76.68%)

--- 0198🟡House_Robber/solution.swift
+++ 0198🟡House_Robber/solution.swift
@@ -1,12 +1,18 @@
 class Solution {
+    private func rob(_ ind: Int, _ nums: [Int], _ memo: inout [Int?]) -> Int {
+        guard ind < nums.count else { return .zero }
+        if let mem = memo[ind] { return mem }
+        let result = max(
+            nums[ind] + rob(ind + 2, nums, &memo),
+            rob(ind + 1, nums, &memo)
+        )
+        memo[ind] = result
+        return result
+    }
+
     func rob(_ nums: [Int]) -> Int {
-        var f = 0
-        var s = 0
-        for num in nums {
-            let c = max(f + num, s)
-            f = s
-            s = c
-        }
-        return s
+        var memo = Array(repeating: Int?.none, count: nums.count)
+        return rob(.zero, nums, &memo)
     }
 }
+
