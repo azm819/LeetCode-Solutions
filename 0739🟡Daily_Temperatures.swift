719927a749da2efddc921e756f540a48c59140d9
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-01-31 06:41:34 +0000

    Sync LeetCode submission - Daily Temperatures (swift)

--- 0739🟡Daily_Temperatures.swift
+++ 0739🟡Daily_Temperatures.swift
@@ -1,14 +1,14 @@
 class Solution {
     func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
         var stack = [(Int, Int)]()
-        var answer = Array(repeating: 0, count: temperatures.count)
+        var result = Array(repeating: 0, count: temperatures.count)
         for (ind, temperature) in temperatures.enumerated() {
             while let (lastT, lastInd) = stack.last, lastT < temperature {
                 stack.removeLast()
-                answer[lastInd] = ind - lastInd
+                result[lastInd] = ind - lastInd
             }
             stack.append((temperature, ind))
         }
-        return answer
+        return result
     }
 }
