003fc1a30d2af77b72b9082faecdcd56e4858cef
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-02-16 06:39:12 +0000

    Sync LeetCode submission Runtime - 243 ms (46.67%), Memory - 24.5 MB (46.67%)

---
+++ 1604🟡Least_Number_of_Unique_Integers_after_K_Removals/solution.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func findLeastNumOfUniqueInts(_ arr: [Int], _ k: Int) -> Int {
+        var counts = [Int: Int]()
+        for int in arr {
+            counts[int, default: .zero] += 1
+        }
+
+        var k = k
+        var result = counts.count
+        for (_, count) in counts.sorted(by: { $0.value < $1.value }) {
+            if k < count {
+                break
+            }
+            k -= count
+            result -= 1
+        }
+        return result
+    }
+}
+
