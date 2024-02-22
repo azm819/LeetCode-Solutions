07e9b01812be970a15b735254571071a3a81bd88
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-02-22 06:56:21 +0000

    Sync LeetCode submission Runtime - 281 ms (100.00%), Memory - 16.1 MB (93.33%)

--- 1039🟢Find_the_Town_Judge/solution.swift
+++ 1039🟢Find_the_Town_Judge/solution.swift
@@ -2,19 +2,17 @@ class Solution {
     func findJudge(_ n: Int, _ trust: [[Int]]) -> Int {
         var arr = Array(repeating: 0, count: n)
         for tr in trust {
-            arr[tr[0] - 1] = -n
+            arr[tr[0] - 1] -= 1
             arr[tr[1] - 1] += 1
         }
-        var res: Int? = nil
-        for i in 0..<n {
-            if arr[i] == n - 1 {
-                if res == nil {
-                    res = i + 1
-                } else {
-                    return -1
-                }
+        var res: Int?
+        for i in 0 ..< n where arr[i] == n - 1 {
+            if res == nil {
+                res = i + 1
+            } else {
+                return -1
             }
         }
-        return res == nil ? -1 : res!
+        return res ?? -1
     }
 }
