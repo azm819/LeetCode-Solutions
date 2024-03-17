0bbdb1ab886480aa54ef80c7761001e1d8892947
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-03-17 10:06:18 +0000

    Sync LeetCode submission Runtime - 34 ms (27.50%), Memory - 16.1 MB (98.60%)

--- 0057🟡Insert_Interval/solution.swift
+++ 0057🟡Insert_Interval/solution.swift
@@ -1,38 +1,29 @@
 class Solution {
-    func intersects(_ f: [Int], _ s: [Int]) -> Bool {
-        return s[0] <= f[0] && f[0] <= s[1] || s[0] <= f[1] && f[1] <= s[1] || f[0] <= s[0] && s[0] <= f[1] || f[0] <= s[1] && s[1] <= f[1]
+    private func intersects(_ f: [Int], _ s: [Int]) -> Bool {
+        return s[0] <= f[0] && f[0] <= s[1] || s[0] <= f[1] && f[1] <= s[1] ||
+            f[0] <= s[0] && s[0] <= f[1] || f[0] <= s[1] && s[1] <= f[1]
     }
 
     func insert(_ intervals: [[Int]], _ newInterval: [Int]) -> [[Int]] {
-        guard !intervals.isEmpty else { return [newInterval] }
-        var result = [[Int]]()
-        var minI = newInterval[0]
-        var maxI = newInterval[1]
-        var old = false
-        var wasIn = false
-        for int in intervals {
-            let isIn = intersects(int, newInterval)
-            if isIn {
-                wasIn = true
-                minI = min(minI, int[0])
-                maxI = max(maxI, int[1])
-            } else {
-                if isIn != old {
-                    result.append([minI, maxI])
+        var result = [newInterval]
+        for interval in intervals {
+            if let lastInterval = result.popLast() {
+                if intersects(lastInterval, interval) {
+                    result.append([min(lastInterval[0], interval[0]), max(lastInterval[1], interval[1])])
+                } else {
+                    if lastInterval[0] < interval[0] {
+                        result.append(lastInterval)
+                        result.append(interval)
+                    } else {
+                        result.append(interval)
+                        result.append(lastInterval)
+                    }
                 }
-                result.append(int)
+            } else {
+                result.append(interval)
             }
-            old = isIn
-        }
-        if old {
-            result.append([minI, maxI])
-        } else if !wasIn {
-            var i = 0
-            while i < intervals.count && intervals[i][0] < newInterval[0] {
-                i += 1
-            }
-            result.insert(newInterval, at: i)
         }
         return result
     }
 }
+
