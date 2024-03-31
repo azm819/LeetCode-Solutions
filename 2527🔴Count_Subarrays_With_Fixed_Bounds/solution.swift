d4da4212ca2f2538aedb02927246875e84be6894
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-03-31 11:00:13 +0000

    Sync LeetCode submission Runtime - 329 ms (22.28%), Memory - 19.6 MB (30.43%)

--- 2527🔴Count_Subarrays_With_Fixed_Bounds/solution.swift
+++ 2527🔴Count_Subarrays_With_Fixed_Bounds/solution.swift
@@ -1,56 +1,57 @@
 class Solution {
-    private func countSubarrays(
-        _ nums: [Int],
-        _ minK: Int,
-        _ maxK: Int,
-        _ from: Int,
-        _ to: Int
-    ) -> Int {
-        guard to >= from else { return .zero }
-        if from == to { return minK == maxK && nums[from] == minK ? 1 : .zero }
-        var result = 0
+    func countSubarrays(_ nums: [Int], _ minK: Int, _ maxK: Int) -> Int {
+        var baseL = 0
+        var l = 0
+        var r = 0
         var minCount = 0
         var maxCount = 0
-        var lastMin = from
-        var lastMax = from
-        var toI = from
-        var lastTo = from
-        while toI <= to {
-            if nums[toI] == minK {
-                minCount += 1
-                lastMin = toI
-            }
-            if nums[toI] == maxK {
-                maxCount += 1
-                lastMax = toI
-            }
-            if minCount > .zero && maxCount > .zero {
-                let isMin = nums[toI] == minK
-                let fromI = isMin ? lastMax : lastMin
-                result += (fromI - lastTo + 1) * (to - toI + 1)
-                lastTo = fromI + 1
-                minCount = isMin ? 1 : .zero
-                maxCount = isMin ? .zero : 1
-            }
-            toI += 1
-        }
-        return result
-    }
 
-    func countSubarrays(_ nums: [Int], _ minK: Int, _ maxK: Int) -> Int {
         var result = 0
-        var from = 0
-        var to = 0
-        while to < nums.count {
-            if nums[to] <= maxK && nums[to] >= minK {
-                to += 1
+        while r < nums.count {
+            if nums[r] < minK || nums[r] > maxK {
+                r += 1
+                baseL = r
+                l = r
+                minCount = 0
+                maxCount = 0
             } else {
-                result += countSubarrays(nums, minK, maxK, from, to - 1)
-                to += 1
-                from = to
+                if nums[r] == minK {
+                    minCount += 1
+                }
+                if nums[r] == maxK {
+                    maxCount += 1
+                }
+
+                if minCount >= 1 && maxCount >= 1 {
+                    while minCount > 1 || maxCount > 1 {
+                        if nums[l] == minK {
+                            if minCount > 1 {
+                                minCount -= 1
+                            } else {
+                                break
+                            }
+                        } else if nums[l] == maxK {
+                            if maxCount > 1 {
+                                maxCount -= 1
+                            } else {
+                                break
+                            }
+                        }
+
+                        l += 1
+                    }
+
+                    while nums[l] > minK && nums[l] < maxK {
+                        l += 1
+                    }
+
+                    result += l - baseL + 1
+                }
+
+                r += 1
             }
         }
-        result += countSubarrays(nums, minK, maxK, from, to - 1)
+
         return result
     }
 }
