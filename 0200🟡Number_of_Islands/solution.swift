668c7e486269657944a438212a1f322bcaf53957
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-04-19 10:13:15 +0000

    Sync LeetCode submission Runtime - 158 ms (53.66%), Memory - 18.3 MB (83.44%)

--- 0200🟡Number_of_Islands/solution.swift
+++ 0200🟡Number_of_Islands/solution.swift
@@ -4,15 +4,16 @@ class Solution {
         let m = grid[0].count
         var visited = Array(repeating: Array(repeating: false, count: m), count: n)
         var result = 0
-        for i in 0..<n {
-            for j in 0..<m {
+        for i in 0 ..< n {
+            for j in 0 ..< m {
                 if visited[i][j] { continue }
+
                 if grid[i][j] == "1" {
                     result += 1
                     var queue = [(i, j)]
-                    while !queue.isEmpty {
-                        let (x, y) = queue.popLast()!
+                    while let (x, y) = queue.popLast() {
                         if visited[x][y] { continue }
+
                         if x > 0 && grid[x - 1][y] == "1" {
                             queue.append((x - 1, y))
                         }
@@ -25,10 +26,10 @@ class Solution {
                         if y < m - 1 && grid[x][y + 1] == "1" {
                             queue.append((x, y + 1))
                         }
+
                         visited[x][y] = true
                     }
                 }
-                visited[i][j] = true
             }
         }
         return result
