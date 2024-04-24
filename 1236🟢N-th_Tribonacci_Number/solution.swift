2c215518c28580cf6ab187a88acec702523188d0
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-04-24 09:08:50 +0000

    Sync LeetCode submission Runtime - 4 ms (56.35%), Memory - 15 MB (82.96%)

--- 1236🟢N-th_Tribonacci_Number/solution.swift
+++ 1236🟢N-th_Tribonacci_Number/solution.swift
@@ -1,20 +1,18 @@
 class Solution {
+    private static var memo = [0, 1, 1]
+
     func tribonacci(_ n: Int) -> Int {
-        guard n > 0 else { return 0 }
-        guard n > 1 else { return 1 }
-        guard n > 2 else { return 1 }
-        var a = 0
-        var b = 1
-        var c = 1
-        var i = 2
-        while i < n {
+        var a = Solution.memo[Solution.memo.count - 3]
+        var b = Solution.memo[Solution.memo.count - 2]
+        var c = Solution.memo[Solution.memo.count - 1]
+        while Solution.memo.count <= n {
             let oldC = c
             let oldB = b
             c += a + b
             b = oldC
             a = oldB
-            i += 1
+            Solution.memo.append(c)
         }
-        return c
+        return Solution.memo[n]
     }
 }
