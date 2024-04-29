7b974bce9d4b105358682c9dd828bbe8a71faaa4
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-04-29 10:26:08 +0000

    Sync LeetCode submission Runtime - 7 ms (92.05%), Memory - 20.2 MB (5.68%)

--- 0072🟡Edit_Distance/solution.swift
+++ 0072🟡Edit_Distance/solution.swift
@@ -4,14 +4,16 @@ class Solution {
         _ j: Int,
         _ word1: [Character],
         _ word2: [Character],
-        _ distances: inout [[Int]]
+        _ distances: inout [[Int?]]
     ) -> Int {
         guard i > 0 && j > 0 else { return max(i, j) }
-        guard distances[i][j] == -1 else { return distances[i][j] }
+        if let distance = distances[i][j] { return distance }
+
+        let result: Int
         if word1[i - 1] == word2[j - 1] {
-            distances[i][j] = getDistance(i - 1, j - 1, word1, word2, &distances)
+            result = getDistance(i - 1, j - 1, word1, word2, &distances)
         } else {
-            distances[i][j] = min(
+            result = min(
                 min(
                     getDistance(i, j - 1, word1, word2, &distances),
                     getDistance(i - 1, j, word1, word2, &distances)
@@ -19,13 +21,15 @@ class Solution {
                 getDistance(i - 1, j - 1, word1, word2, &distances)
             ) + 1
         }
-        return distances[i][j]
+
+        distances[i][j] = result
+        return result
     }
 
     func minDistance(_ word1: String, _ word2: String) -> Int {
         guard word1 != word2 else { return .zero }
         guard !word1.isEmpty && !word2.isEmpty else { return max(word1.count, word2.count) }
-        var distances = Array(repeating: Array(repeating: -1, count: word2.count + 1), count: word1.count + 1)
+        var distances = Array(repeating: Array(repeating: Int?.none, count: word2.count + 1), count: word1.count + 1)
         return getDistance(word1.count, word2.count, Array(word1), Array(word2), &distances)
     }
 }
