be45ed223d9be407cb406baf8cc54a660922ae51
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-04-30 10:20:54 +0000

    Sync LeetCode submission Runtime - 328 ms (100.00%), Memory - 16.4 MB (100.00%)

---
+++ 2044🟡Number_of_Wonderful_Substrings/solution.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func wonderfulSubstrings(_ word: String) -> Int {
+        let firstAsciiValue = Character("a").asciiValue!
+        
+        var maskCount = [Int: Int]()
+        var curMask = 0
+        maskCount[curMask, default: .zero] += 1
+        var result = 0
+        for char in word {
+            let offset = char.asciiValue! - firstAsciiValue
+            curMask ^= 1 << offset
+            
+            result += maskCount[curMask, default: .zero]
+            maskCount[curMask, default: .zero] += 1 // number of substrings with the same mask
+
+            for i in 0 ..< 10 {
+                result += maskCount[curMask ^ (1 << i), default: .zero] // number of substrings with a mask that differs in one position
+            }
+        }
+
+        return result
+    }
+}
