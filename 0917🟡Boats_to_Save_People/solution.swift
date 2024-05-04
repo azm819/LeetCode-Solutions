2eda2589c79aac4bca51aa6f9a6f32303ed5ff0c
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-05-04 10:17:01 +0000

    Sync LeetCode submission Runtime - 164 ms (19.25%), Memory - 17.8 MB (50.25%)

--- 0917🟡Boats_to_Save_People/solution.swift
+++ 0917🟡Boats_to_Save_People/solution.swift
@@ -1,17 +1,17 @@
 class Solution {
     func numRescueBoats(_ people: [Int], _ limit: Int) -> Int {
-        let people = people.sorted(by: >)
+        let people = people.sorted()
         var left = 0
         var right = people.count - 1
         var result = 0
         while left <= right {
-            let secondPer = limit - people[left]
-            result += 1
-            if left != right && people[right] <= secondPer {
-                right -= 1
+            if people[left] + people[right] <= limit {
+                left += 1
             }
-            left += 1
+            right -= 1
+            result += 1
         }
         return result
     }
 }
+
