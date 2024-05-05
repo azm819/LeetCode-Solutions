1db8f7825daac7582be4b616eb2e444412a90cf5
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-05-05 10:30:58 +0000

    Sync LeetCode submission Runtime - 12 ms (43.85%), Memory - 15.9 MB (7.28%)

---
+++ 0237🟡Delete_Node_in_a_Linked_List/solution.swift
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func deleteNode(_ node: ListNode?) {
+        if let nextNode = node?.next {
+            node?.val = nextNode.val
+            node?.next = nextNode.next
+        }
+    }
+}
