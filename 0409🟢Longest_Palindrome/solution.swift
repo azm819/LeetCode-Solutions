5ca252b40ac02cb5c1db31737d3a4b986c5131e0
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-06-04 06:35:39 +0000

    Sync LeetCode submission Runtime - 5 ms (69.57%), Memory - 16.4 MB (40.22%)

--- 0409🟢Longest_Palindrome/solution.swift
+++ 0409🟢Longest_Palindrome/solution.swift
@@ -1,18 +1,18 @@
 class Solution {
     func longestPalindrome(_ s: String) -> Int {
-        var map = [Character: Int]()
-        for i in s {
-            map[i] = (map[i] ?? .zero) + 1
+        var counts = [Character: Int]()
+        for ch in s {
+            counts[ch, default: .zero] += 1
         }
+
         var result = 0
-        var hasnotMid = true
-        for i in map {
-            result += i.value / 2 * 2
-            if hasnotMid && i.value % 2 == 1 {
-                result += 1
-                hasnotMid = false
+        var hasMid = false
+        for (_, count) in counts {
+            result += count / 2 * 2
+            if count % 2 == 1 {
+               hasMid = true 
             }
         }
-        return result
+        return hasMid ? result + 1 : result
     }
 }
