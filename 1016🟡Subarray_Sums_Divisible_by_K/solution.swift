e2d7bfd7968e807e5b5d31fbb6eefde83f26418b
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-06-09 09:44:23 +0000

    Sync LeetCode submission Runtime - 103 ms (74.24%), Memory - 16.1 MB (92.05%)

--- 1016🟡Subarray_Sums_Divisible_by_K/solution.swift
+++ 1016🟡Subarray_Sums_Divisible_by_K/solution.swift
@@ -2,12 +2,12 @@ class Solution {
     func subarraysDivByK(_ nums: [Int], _ k: Int) -> Int {
         var mods = Array(repeating: 0, count: k)
         mods[0] = 1
-        var prevSum = 0
+        var curMod = 0
         var result = 0
         for num in nums {
-            prevSum = ((prevSum + num) % k + k) % k
-            result += mods[prevSum]
-            mods[prevSum] += 1
+            curMod = ((curMod + num) % k + k) % k
+            result += mods[curMod]
+            mods[curMod] += 1
         }
         return result
     }
