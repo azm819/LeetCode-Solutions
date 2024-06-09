06c2d4881845ec9ab8b7657d502f84be2dc5726d
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-06-09 10:17:23 +0000

    Sync LeetCode submission Runtime - 16 ms (52.83%), Memory - 16.1 MB (67.92%)

---
+++ 1651🟢Shuffle_String/solution.swift
@@ -0,0 +1,10 @@
+class Solution {
+    func restoreString(_ s: String, _ indices: [Int]) -> String {
+        var result: [Character] = Array(repeating: " ", count: s.count)
+        for (char, index) in zip(s, indices) {
+            result[index] = char
+        }
+        return String(result)
+    }
+}
+
