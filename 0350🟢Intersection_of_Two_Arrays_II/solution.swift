2684c9a1328a296d5e0726086e267fe05178fdd0
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-07-02 06:42:24 +0000

    Sync LeetCode submission Runtime - 10 ms (50.82%), Memory - 16.1 MB (57.95%)

--- 0350🟢Intersection_of_Two_Arrays_II/solution.swift
+++ 0350🟢Intersection_of_Two_Arrays_II/solution.swift
@@ -1,36 +1,15 @@
 class Solution {
-    private func search(_ nums: [Int], _ target: Int, _ selected: [Bool]) -> Int {
-        var l = 0
-        var r = nums.count - 1
-        while l < r {
-            let c = (l + r) / 2
-            if nums[c] == target {
-                if selected[c] {
-                    l = c + 1
-                } else {
-                    r = c
-                }
-            } else if nums[c] < target {
-                l = c + 1
-            } else {
-                r = c - 1
-            }
-        }
-        return nums[l] == target && !selected[l] ? l : -1
-    }
-
     func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
-        let nums2 = nums2.sorted()
-        print(nums2)
-        var selected = Array(repeating: false, count: nums2.count)
-        return nums1.filter { num in
-            let ind = search(nums2, num, selected)
-            if ind != -1 {
-                selected[ind] = true
-                return true
-            } else {
-                return false
-            }
+        var counts = [Int: Int]()
+        for num in nums1 {
+            counts[num, default: .zero] += 1
+        }
+
+        return nums2.filter { num in
+            guard let count = counts[num] else { return false }
+            counts[num] = count > 1 ? count - 1 : nil
+            return true
         }
     }
 }
+
