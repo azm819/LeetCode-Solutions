7b6a84f36cb29c61ddf4b5f3d5b3e72e5ac3a950
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-07-25 09:49:26 +0000

    Sync LeetCode submission Runtime - 509 ms (60.36%), Memory - 23 MB (20.49%)

--- 0948🟡Sort_an_Array/solution.swift
+++ 0948🟡Sort_an_Array/solution.swift
@@ -1,41 +1,37 @@
 class Solution {
-    private func sortArray(_ nums: inout [Int], _ from: Int, _ to: Int) {
-        guard from != to else { return }
-        guard from + 1 < to else {
-            if nums[to] < nums[from] {
-                nums.swapAt(from, to)
-            }
-            return
-        }
-        let c = (from + to) / 2
-        sortArray(&nums, from, c)
-        sortArray(&nums, c + 1, to)
-        var result = [Int]()
-        var f = from
-        var s = c + 1
-        while f <= c && s <= to {
-            if nums[f] > nums[s] {
-                result.append(nums[s])
-                s += 1
-            } else {
-                result.append(nums[f])
-                f += 1
-            }
-        }
-        while f <= c {
-            result.append(nums[f])
-            f += 1
-        }
-        while s <= to {
-            result.append(nums[s])
-            s += 1
-        }
-        nums.replaceSubrange(from...to, with: result)
-    }
-
     func sortArray(_ nums: [Int]) -> [Int] {
-        var res = nums
-        sortArray(&res, 0, res.count - 1)
-        return res
+        nums.mergeSorted(from: nums.startIndex, to: nums.index(before: nums.endIndex))
+    }
+}
+
+private extension Array where Element: Comparable {
+    func mergeSorted(from: Index, to: Index) -> [Element] {
+        guard from < to else {
+            if from == to {
+                return [self[from]]
+            }
+            return []
+        }
+
+        let mid = (from + to) / 2
+        let leftSorted = mergeSorted(from: from, to: mid)
+        let rightSorted = mergeSorted(from: mid + 1, to: to)
+        var l = leftSorted.startIndex
+        var r = rightSorted.startIndex
+        var sorted = [Element]()
+        while l != leftSorted.endIndex && r != rightSorted.endIndex {
+            if leftSorted[l] < rightSorted[r] {
+                sorted.append(leftSorted[l])
+                l = leftSorted.index(after: l)
+            } else {
+                sorted.append(rightSorted[r])
+                r = rightSorted.index(after: r)
+            }
+        }
+
+        sorted.append(contentsOf: leftSorted[l...])
+        sorted.append(contentsOf: rightSorted[r...])
+
+        return sorted
     }
 }
