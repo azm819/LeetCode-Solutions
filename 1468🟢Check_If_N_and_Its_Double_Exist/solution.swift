f71633ab0bc315c8c2ee4c421e8fadbf2e61f042
Azamat Farkhiev <a.farkhiev@tinkoff.ru>
2024-12-01 08:57:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (14.55%)

--- 1468🟢Check_If_N_and_Its_Double_Exist/solution.swift
+++ 1468🟢Check_If_N_and_Its_Double_Exist/solution.swift
@@ -1,28 +1,13 @@
 class Solution {
-    private func search(_ nums: [Int], _ target: Int) -> Int {
-        var l = 0
-        var r = nums.count - 1
-        while l <= r {
-            let c = (l + r) / 2
-            if nums[c] == target {
-                return c
-            } else if nums[c] < target {
-                l = c + 1
-            } else {
-                r = c - 1
-            }
-        }
-        return -1
-    }
-
     func checkIfExist(_ arr: [Int]) -> Bool {
-        let arr = arr.sorted()
-        for i in 0..<arr.count {
-            let searchRes = search(arr, arr[i] * 2)
-            if searchRes != -1 && searchRes != i {
+        var numsSet = Set<Int>()
+        for num in arr {
+            if numsSet.contains(num * 2) || num % 2 == .zero && numsSet.contains(num / 2) {
                 return true
             }
+            numsSet.insert(num)
         }
         return false
     }
 }
+
